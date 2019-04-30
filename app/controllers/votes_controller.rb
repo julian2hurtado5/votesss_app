a15018da28d598e8ae0657e42d540b977060b617
Jorge Betancur <jabetancur12@gmail.com>
2019-04-30 18:16:15 -0500

    Centrado de vistas

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,6 @@
 class VotesController < ApplicationController
   def create
+    p params
     language = Language.find(params[:language_id])
     language.votes.create(user: current_user)
     redirect_to root_path
