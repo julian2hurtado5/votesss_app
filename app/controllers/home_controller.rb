73ea83b4e88b2cc74503a5b77c7ada1f2e6026fd
Jorge Betancur <jabetancur12@gmail.com>
2019-04-29 22:10:55 -0500

    Interfaz lista

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+    def index
+        @languages = Language.all
+    end
+
+end
---
+++ db/migrate/20190430012417_add_imgurl_to_languages.rb
@@ -0,0 +1,5 @@
+class AddImgurlToLanguages < ActiveRecord::Migration[5.2]
+  def change
+    add_column :languages, :imgurl, :string
+  end
+end
